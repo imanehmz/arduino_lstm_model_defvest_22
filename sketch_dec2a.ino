683aefcaf22f05b6dd1028ef74c7915d5c28c63b
islam36 <jm_boumendjel@esi.dz>
2022-12-02 08:23:28 +0100

    multichannel gaz sensor code

--- sketch_dec2a.ino
+++ sketch_dec2a.ino
@@ -1,26 +1,68 @@
-#include <DHT.h>
-int pin = A1;
-
-DHT dht(pin, DHT22);
-
-void setup() {
-  Serial.begin(9600);
-
-  dht.begin();
+// Read Data from Grove - Multichannel Gas Sensor
+#include <Wire.h>
+#include "MutichannelGasSensor.h"
+ 
+void setup()
+{
+    Serial.begin(115200);  // start serial for output
+    Serial.println("power on!");
+    gas.begin(0x04);//the default I2C address of the slave is 0x04
+    gas.powerOn();
+    Serial.print("Firmware Version = ");
+    Serial.println(gas.getVersion());
 }
-
-int val;
-
-
-void loop() {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  Serial.print("humidity: ");
-  Serial.println(h);
-
-  Serial.print("temperature: ");
-  Serial.println(t);
-
-  delay(2000);
+ 
+void loop()
+{
+    float c;
+ 
+    c = gas.measure_NH3();
+    Serial.print("The concentration of NH3 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_CO();
+    Serial.print("The concentration of CO is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_NO2();
+    Serial.print("The concentration of NO2 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_C3H8();
+    Serial.print("The concentration of C3H8 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_C4H10();
+    Serial.print("The concentration of C4H10 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_CH4();
+    Serial.print("The concentration of CH4 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_H2();
+    Serial.print("The concentration of H2 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    c = gas.measure_C2H5OH();
+    Serial.print("The concentration of C2H5OH is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+ 
+    delay(1000);
 }
